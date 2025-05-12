692ab650e730d199866f0fe50506188737d37cc9
ayush01-dev <111ayushh@gmail.com>
2025-05-12 16:38:37 +0530

    new

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ import utils
 import sys
 import uvicorn
 from starlette.middleware.sessions import SessionMiddleware
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables
+# Use os.path.join for file paths
+config_dir = os.path.join(os.path.dirname(_file_), "config")
+os.makedirs(config_dir, exist_ok=True)
+
 
 
 
